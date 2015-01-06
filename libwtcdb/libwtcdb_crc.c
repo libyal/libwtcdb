526222324ae63aa0a72a926d99b5aa893b622d81
Joachim Metz <joachim.metz@gmail.com>
2015-01-06 07:35:24 +0100

    Applied updates.

--- libwtcdb/libwtcdb_crc.c
+++ libwtcdb/libwtcdb_crc.c
@@ -1,7 +1,7 @@
 /*
  * CRC-64 functions
  *
- * Copyright (C) 2010-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
