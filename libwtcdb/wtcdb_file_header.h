46a98ae677a79e0583fbc895de29706744cecd3e
Joachim Metz <joachim.metz@gmail.com>
2019-01-13 16:00:20 +0100

    Applied updates

--- libwtcdb/wtcdb_file_header.h
+++ libwtcdb/wtcdb_file_header.h
@@ -1,7 +1,7 @@
 /*
  * The file header definition of a Windows Explorer thumbnail cache database file
  *
- * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2019, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
