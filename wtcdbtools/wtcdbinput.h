d9ad125b5b0f7843240a89a6df0a3c6a9468333e
Joachim Metz <joachim.metz@gmail.com>
2022-02-28 06:41:18 +0100

    Applied updates

--- wtcdbtools/wtcdbinput.h
+++ wtcdbtools/wtcdbinput.h
@@ -1,7 +1,7 @@
 /*
  * Common input functions for the wtcdbtools
  *
- * Copyright (C) 2010-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
